e8bd07dbe5585ce7675a30caf539c1c0323ac0f7
✗ 𝐈𝐭𝐚𝐜𝐡𝐢...♞ <nobody0909zz@gmail.com>
2026-09-08 13:19:28 +0200

    Update bot number code in settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.author = '𝐈𝐭𝐚𝐜𝐡𝐢♞'
 global.multiplier = 85                                                                                                                                      
 
 // اكتب رقمك هنا الي عايز تربط بي البوت من دون + او فواصل                     
-global.botNumberCode = "+212694571335"
+global.botNumberCode = "+229927142037"
 
 // ========== القنوات ==========                                              
 global.ch = {                                                                   
