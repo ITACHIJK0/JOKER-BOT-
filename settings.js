c1826dc5609d201bc2d70d800d5ea592ede14e11
✗ 𝐈𝐭𝐚𝐜𝐡𝐢...♞ <nobody0909zz@gmail.com>
2026-09-11 21:25:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.author = '𝐈𝐭𝐚𝐜hi♞'
 global.multiplier = 85                                                                                                                                      
 
 // اكتب رقمك هنا الي عايز تربط بي البوت من دون + او فواصل                     
-global.botNumberCode = "249927142037"
+global.botNumberCode = "212694571335"
 
 // ========== القنوات ==========                                              
 global.ch = {                                                                   
